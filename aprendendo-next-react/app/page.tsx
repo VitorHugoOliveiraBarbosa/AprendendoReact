20e5de582820ea4054e86f3984766df8d05f3f02
Vitor Hugo <107222031+VitorHugoOliveiraBarbosa@users.noreply.github.com>
2024-12-31 20:36:58 +0000

    ola mundo

---
+++ aprendendo-next-react/app/page.tsx
@@ -0,0 +1,6 @@
+function Ola(){
+  return <div className=" container h-dvh mx-auto px-4 text-center bg-cyan-200 hover:bg-cyan-50 ">
+    <h1 className="text-blue-400 hover:text-black">Olá mundo!</h1>
+  </div>
+}
+export default Ola;
